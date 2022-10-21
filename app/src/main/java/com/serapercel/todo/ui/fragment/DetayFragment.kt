e3f61ecf56b0354cf561a94c2351ea44ed1e9adf
Serap <ercelserap@gmail.com>
2022-10-21 19:12:14 +0300

    Add fragments

---
+++ app/src/main/java/com/serapercel/todo/ui/fragment/DetayFragment.kt
@@ -0,0 +1,16 @@
+package com.serapercel.todo.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.serapercel.todo.R
+
+class DetayFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_detay, container, false)
+    }
+}
